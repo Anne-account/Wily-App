007e6ec6f042fb41c3ccc8823f96c4f4c7cb2cb0
Anne-account <73730762+Anne-account@users.noreply.github.com>
2021-10-13 17:06:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+// add your database
+/*
+var firebaseConfig = {
+  apiKey: "AIzaSyCSvK-SsEwvO7rHrf0WLiqsO0o5S7pI9-8",
+    authDomain: "wily-app-86cec.firebaseapp.com",
+    databaseURL: "https://wily-app-86cec.firebaseio.com",
+    projectId: "wily-app-86cec",
+    storageBucket: "wily-app-86cec.appspot.com",
+    messagingSenderId: "436866087439",
+    appId: "1:436866087439:web:1a3bcf134182eb6559017a"
+};*/
+// Initialize Firebase
+if(!firebase.apps.length){
+firebase.initializeApp(firebaseConfig);
+}
+export default firebase.firestore();
